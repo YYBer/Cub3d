b22018f118114631d5ff2314a28e3c38a5a7daaf
YYBer_42Kranken <liyuanying225@gmail.com>
2023-08-02 22:23:20 +0200

    add texture.h

---
+++ texture.h
@@ -0,0 +1,11 @@
+#ifndef TEXTURE_H
+# define TEXTURE_H
+
+# define WALL1 "./texture/wall1.png"
+# define WALL2 "./texture/wall2.png"
+# define WALL3 "./texture/wall3.png"
+# define WALL4 "./texture/wall4.png"
+
+
+
+#endif
