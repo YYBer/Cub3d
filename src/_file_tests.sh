54edb9b5a4755a421081f5cc289205c39a3011a3
Graham Booth <graham.r.booth@gmail.com>
2023-08-18 09:40:38 +0200

    removed unnecessary files

--- src/_file_tests.sh
+++ src/_file_tests.sh
@@ -5,46 +5,46 @@ log_file="_file_tests.log"
 
 # List of input files
 input_files=(
-    "maps/everything_valid.cub" #TESTED
-    "maps/map_complex_leading_spaces_1.cub" #TESTED
-    "maps/map_complex_leading_spaces_2.cub" #TESTED
-    "maps/map_complex_leading_spaces_differing_line_lengths.cub" #TESTED
-    "maps/map_complex_subject.cub" #TESTED
-    "maps/map_none.cub" #TESTED
-    "maps/map_simple_all_walls.cub" #TESTED
-    "maps/map_simple_extra_position.cub" #TESTED
-    "maps/map_simple_illegal_char.cub" #TESTED
-    "maps/map_simple_large.cub" #TESTED
-    "maps/map_simple_no_position.cub" #TESTED
-    "maps/map_simple_rect_wide.cub" #TESTED
-    "maps/map_simple_smallest_valid.cub" #TESTED
-    "maps/map_single_0.cub" #TESTED
-    "maps/map_single_1.cub" #TESTED
-    "maps/map_single_illegal_char.cub" #TESTED
-    "maps/map_wall_jean.cub" #TESTED
-    "maps/map_wall_slack_1_valid.cub" #TESTED
-    "maps/map_wall_slack_2_valid.cub" #TESTED
-    "maps/map_wall_slack_3_invalid.cub" #TESTED
-    "maps/preamble_color_double_comma.cub" #TESTED
-    "maps/preamble_color_extra_comma.cub" #TESTED
-    "maps/preamble_color_invalid_value_1.cub" #TESTED
-    "maps/preamble_color_invalid_value_2.cub" #TESTED
-    "maps/preamble_color_invalid_value_3.cub" #TESTED
-    "maps/preamble_color_omitted_comma.cub" #TESTED
-    "maps/preamble_color_omitted_prefix.cub" #TESTED
-    "maps/preamble_color_omitted_values.cub" #TESTED
-    "maps/preamble_color_repeated_prefix.cub" #TESTED
-    "maps/preamble_newlines_at_end_invalid.cub" #TESTED
-    "maps/preamble_newlines_none.cub" #TESTED
-    "maps/preamble_newlines_random.cub" #TESTED
-    "maps/preamble_random_order.cub" #TESTED
-    "maps/preamble_spaces_none.cub" #TESTED
-    "maps/preamble_spaces_random.cub" #TESTED
-    "maps/preamble_texture_double_repeated_prefix.cub" #TESTED
-    "maps/preamble_texture_invalid.cub" #TESTED
-    "maps/preamble_texture_omitted_path.cub" #TESTED
-    "maps/preamble_texture_omitted_prefix.cub" #TESTED 
-    "maps/preamble_texture_repeated_prefix.cub" #TESTED
+    "maps/everything_valid.cub"
+    "maps/map_complex_leading_spaces_1.cub"
+    "maps/map_complex_leading_spaces_2.cub"
+    "maps/map_complex_leading_spaces_differing_line_lengths.cub"
+    "maps/map_complex_subject.cub"
+    "maps/map_none.cub"
+    "maps/map_simple_all_walls.cub"
+    "maps/map_simple_extra_position.cub"
+    "maps/map_simple_illegal_char.cub"
+    "maps/map_simple_large.cub"
+    "maps/map_simple_no_position.cub"
+    "maps/map_simple_rect_wide.cub"
+    "maps/map_simple_smallest_valid.cub"
+    "maps/map_single_0.cub"
+    "maps/map_single_1.cub"
+    "maps/map_single_illegal_char.cub"
+    "maps/map_wall_jean.cub"
+    "maps/map_wall_slack_1_valid.cub"
+    "maps/map_wall_slack_2_valid.cub"
+    "maps/map_wall_slack_3_invalid.cub"
+    "maps/preamble_color_double_comma.cub"
+    "maps/preamble_color_extra_comma.cub"
+    "maps/preamble_color_invalid_value_1.cub"
+    "maps/preamble_color_invalid_value_2.cub"
+    "maps/preamble_color_invalid_value_3.cub"
+    "maps/preamble_color_omitted_comma.cub"
+    "maps/preamble_color_omitted_prefix.cub"
+    "maps/preamble_color_omitted_values.cub"
+    "maps/preamble_color_repeated_prefix.cub"
+    "maps/preamble_newlines_at_end_invalid.cub"
+    "maps/preamble_newlines_none.cub"
+    "maps/preamble_newlines_random.cub"
+    "maps/preamble_random_order.cub"
+    "maps/preamble_spaces_none.cub"
+    "maps/preamble_spaces_random.cub"
+    "maps/preamble_texture_double_repeated_prefix.cub"
+    "maps/preamble_texture_invalid.cub"
+    "maps/preamble_texture_omitted_path.cub"
+    "maps/preamble_texture_omitted_prefix.cub" 
+    "maps/preamble_texture_repeated_prefix.cub"
 )
 
 # Clear the log file if it exists
@@ -53,6 +53,7 @@ input_files=(
 # Loop through each input file and run ./cub3d, logging the result
 for file in "${input_files[@]}"; do
     echo "Running $file" >> "$log_file"
-    ./cub3d "$file" 2>&1 | sed 's/^/  /' >> "$log_file"
+    # remove "test" to test graphically with maps
+    ./cub3d "$file" "test" 2>&1 | sed 's/^/  /' >> "$log_file"
     echo "--------------------------------------------------" >> "$log_file"
 done
